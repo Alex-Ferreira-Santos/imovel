bf64e90152f0a5f2f7aaeffe85946234ace4bbe0
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-04-11 12:09:40 -0300

    Imovel component

--- src/components/Imovel.js
+++ src/components/Imovel.js
@@ -5,11 +5,13 @@ import styles from '../styles/imovel'
 function Imovel() {
   return(
       <View style={styles.container}>
-            <View style={styles.badge}>
-                <Text style={styles.type}>Casa</Text>
-            </View>
             <Image style={styles.image}/>
-            <Text style={styles.finalidade}>Venda - <Text style={styles.value}>R$300000,00</Text></Text>
+            <View style={styles.data}>
+              <Text style={styles.title}>Casa 2 quartos a venda</Text>
+              <Text style={styles.price}>R$300000,00</Text>
+              <Text style={styles.localType}>Venda - Casa</Text>
+              <Text style={styles.localType}>Rio de Janeiro, RJ</Text>
+            </View>       
       </View>
   );
 }
--- src/styles/imovel.js
+++ src/styles/imovel.js
@@ -1,38 +1,36 @@
-import {StyleSheet} from 'react-native'
+import {StyleSheet,Dimensions} from 'react-native'
+
+const width = Dimensions.get('screen').width
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor:'#8FAFFF',
-        alignItems: 'center',
+        flexDirection:'row',
         width: '95%',
         marginVertical: 10,
         borderRadius: 20,
-        paddingTop: 10,
-    },
-    badge:{
-        backgroundColor: '#A3E1AA',
-        color: 'white',
-        position:'absolute',
-        alignSelf: 'flex-start',
-        zIndex: 1,
-        left: 9,
-        top: 10,
+        borderColor: '#15B025',
+        borderWidth: 1,
+        alignItems: 'center',
     },
     image:{
-        width: '95%',
-        height: 300,
+        width: width/3.5,
+        height: width/3.5,
+        margin: 10,
         backgroundColor: '#c4c4c4',
         borderRadius: 20
     },
-    type:{
-        fontSize: 30,
+    title:{
+        fontSize: width/20,
+        maxWidth: 280,
     },
-    finalidade:{
-        fontSize: 25,
+    price:{
+        fontSize: width/18,
+        color: '#3B933F'
     },
-    value:{
-        color: '#A9FFAC'
-    }
+    localType:{
+        color: '#898989',
+        fontSize: width/22,
+    },
 })
 
 export default styles
