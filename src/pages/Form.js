31f28d4c0cbf8e5019129302665c054ab230fb83
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-04-12 14:02:08 -0300

    creating form

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,16 +1,62 @@
 import React, { Component } from 'react';
-import {Root, Container,Text,Form,Item,Input,Label } from 'native-base';
+import {TextInput,ScrollView} from 'react-native'
+import {Root,Text,Form,Picker,Item } from 'native-base';
 import {View} from 'react-native'
 import styles from '../styles/form'
+import states from '../../Estados.json'
 export default class GeneralExample extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      buttonAvailable: false,
+    }
+  }
+
   render() {
     return (
       <Root>
-        <Container>
-          <Text style={styles.dados}>Dados</Text>
-          <View style={styles.line}/>
-
-        </Container>
+        <ScrollView>
+            <Text style={styles.dados}>Dados</Text>
+            <View style={styles.line}/>
+            <Form style={styles.form}>
+              <View>
+                <Text style={styles.label}>Título</Text>
+                <TextInput placeholder="Digite o título aqui" style={styles.input} placeholderTextColor="#6C94A0"/>
+              </View>
+              <View style={styles.local}>
+                <View>
+                  <Text style={styles.label}>Estado</Text>
+                  <Item picker>
+                    <Picker 
+                    style={styles.input}
+                    placeholder="Selecione seu estado"
+                    placeholderStyle={{ color: "#6C94A0" }}
+                    placeholderIconColor="#007aff"
+                  >
+                    <Picker.Item label="Wallet" value="key0" />
+                  </Picker>
+                  </Item>
+                </View>
+                <View>
+                  <Text style={styles.label}>Cidade</Text>
+                  <Item picker>
+                    <Picker 
+                    style={styles.input}
+                    placeholder="Selecione sua cidade"
+                    placeholderStyle={{ color: "#6C94A0" }}
+                    placeholderIconColor="#007aff"
+                  >
+                    <Picker.Item label="Wallet" value="key0" />
+                  </Picker>
+                  </Item>
+                  
+                </View>
+              </View>
+              
+              
+            </Form>
+        </ScrollView>
       </Root>
     );
   }
--- src/styles/form.js
+++ src/styles/form.js
@@ -13,6 +13,40 @@ const styles = StyleSheet.create({
         width: width - 20,
         alignSelf: 'center',
         backgroundColor: '#B4B4B4',
+    },
+    label:{
+        fontSize: 25,
+        marginVertical: 5,
+    },
+    input:{
+        borderColor:'#15B025',
+        borderWidth: 1,
+        backgroundColor: '#F0F0F0',
+        width: width/1.1,
+        borderRadius: 20,
+        padding: 0,
+        height: 46,
+        paddingLeft: 10,
+        fontSize: 18,  
+    },
+    estado:{
+        borderColor:'#15B025',
+        borderWidth: 1,
+        backgroundColor: '#F0F0F0',
+        width: width/2.5,
+        borderRadius: 20,
+        padding: 0,
+        height: 46,
+        paddingLeft: 10,
+    },
+    local:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '90%',
+    },
+    form:{
+        alignItems: 'center',
+        marginTop: 10,
     }
 })
 
