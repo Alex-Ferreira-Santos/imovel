8bb848c8cb3680ff333a2d835473e23d4ada7e98
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-04-14 14:15:12 -0300

    creating picture page

--- src/pages/Picture.js
+++ src/pages/Picture.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import {View,Text,TouchableOpacity} from 'react-native'
+import React,{useState} from 'react';
+import {View,Text,TouchableOpacity,StatusBar,Image} from 'react-native'
 import {RNCamera} from 'react-native-camera'
 import styles from '../styles/pictures'
 
 function Picture(){
+    let camera
+    const [image,setImage] = useState('')
+    async function takePicture(){
+        if(camera){
+            const options = {base64:true}
+            const data = await camera.takePictureAsync(options)
+            setImage(data.uri)
+        }
+    }
     return(
         <View style={styles.container}>
-            <RNCamera/>
-            <View>
-                <TouchableOpacity>
+            <StatusBar translucent backgroundColor="transparent"/>
+            <RNCamera 
+                ref={ref => {
+                    camera = ref
+                }}
+                style={styles.preview}
+                type={RNCamera.Constants.Type.back}
+                androidCameraPermissionOptions={{
+                    title: 'Permissão para usar a câmera',
+                    message: 'Nós precisamos da sua permissão para usar a câmera',
+                    buttonPositive: 'Ok',
+                    buttonNegative: 'Cancelar',
+                }}
+            />
+            <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center',backgroundColor:'gray' }}>
+                <TouchableOpacity style={styles.capture} onPress={()=>{takePicture()}}>
                     <Text>Tirar foto</Text>
                 </TouchableOpacity>
             </View>
+            <Image uri={image} style={{width: 100, height: 100}}/>
         </View>
     )
 }
--- src/styles/pictures.js
+++ src/styles/pictures.js
@@ -3,6 +3,21 @@ import {StyleSheet} from 'react-native'
 const styles = StyleSheet.create({
     container: {
         flex:1,
+        backgroundColor: 'black'
+    },
+    preview:{
+        flex:1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    capture:{
+        flex:0,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        padding: 15,
+        paddingHorizontal: 20,
+        alignSelf: 'center',
+        margin: 20,
     }
 })
 
