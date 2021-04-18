7773ceb011b660cd0e37d234a91c1ac8e61dec09
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-04-18 11:24:28 -0300

    change title

--- src/context/PostsContext.js
+++ src/context/PostsContext.js
@@ -13,9 +13,9 @@ export const PostContext = createContext({})
 export function PostProvider(props){
     const [image,setImage] = useState([])
     const [posts,setPosts] = useState([
-        {id:1,title:'Casa de luxo com 5 quartos a venda',price:700000,city:'Rio de Janeiro',state:'Rio de Janeiro',finalidade:'Venda',type:'Casa', adress:'Rua a, 80',description:'Casa com 5 quartos, 2 cozinhas, 2 salas, piscina, jardim e 3 banheiros a venda',image:[{uri:casa1},{uri:casa3},{uri:casa4}]},
-        {id:2,title:'Apartamento 2 quartos para alugar',price:2000,city:'Niterói',state:'Rio de Janeiro',finalidade:'Aluguel',type:'Apartamento',adress:'Rua b, 60',description:'Apartamento para aluguel com 2 quartos, 1 banehiro',image:[{uri:apart1},{uri:apart2},{uri:apart3}]},
-        {id:3,title:'Loja de roupas a venda',price:500000,city:'Volta Redonda',state:'Rio de Janeiro',finalidade:'Venda',type:'Comércio', adress:'Rua c, 40',description:'Loja de roupas a venda, o local tem por volta de 80m quadrados com 2 salas',image:[{uri:loja},{uri:loja2}]},
+        {id:1,title:'Casa de luxo com 5 quartos a venda',price:700000,city:'Rio de Janeiro',state:'Rio de Janeiro',finalidade:'Venda',type:'Casa', adress:'Rua a, 80',description:'Casa com 5 quartos, 2 cozinhas, 2 salas, piscina, jardim e 3 banheiros',image:[{uri:casa1},{uri:casa3},{uri:casa4}]},
+        {id:2,title:'Apartamento 2 quartos',price:2000,city:'Niterói',state:'Rio de Janeiro',finalidade:'Aluguel',type:'Apartamento',adress:'Rua b, 60',description:'Apartamento para aluguel com 2 quartos, 1 banehiro',image:[{uri:apart1},{uri:apart2},{uri:apart3}]},
+        {id:3,title:'Loja de roupas',price:500000,city:'Volta Redonda',state:'Rio de Janeiro',finalidade:'Venda',type:'Comércio', adress:'Rua c, 40',description:'Loja de roupas a venda, o local tem por volta de 80m quadrados com 2 salas',image:[{uri:loja},{uri:loja2}]},
     ])
     const [count,setCount] = useState(1)
     
